0e79445abaa236e128384db03ff4080d425d7198
Jason Down <jdown@igloosoftware.com>
2018-01-03 23:15:57 -0500

    Cleaned up the code in Html Type Provider example with some reusable functions.

--- FunWithFSharpData/HtmlProviderExample.fsx
+++ FunWithFSharpData/HtmlProviderExample.fsx
@@ -10,6 +10,7 @@ let Season2 = @"https://en.wikipedia.org/wiki/Vikings_(season_2)"
 [<Literal>]
 let Season3 = @"https://en.wikipedia.org/wiki/Vikings_(season_3)"
 
+// Seasons 4 and 5 have a different format. Will deal with them later.
 //[<Literal>]
 //let Season4 = @"https://en.wikipedia.org/wiki/Vikings_(season_4)"
 
@@ -23,9 +24,10 @@ type Episode =
       Number      : int
     }
 
-let s1 = new HtmlProvider<Season1>()
-let s1eps = 
-    s1.Tables.Episodes.Rows 
+type EpisodeInfo = HtmlProvider<Season1>
+
+let getEpisodes (seasons : EpisodeInfo) =
+    seasons.Tables.Episodes.Rows
     |> Seq.chunkBySize 2 
     |> Seq.map (fun e -> { Title = e.[0].Title |> String.collect (fun c -> if c = '"' then "" else string c)
                            Season = 1
@@ -33,52 +35,14 @@ let s1eps =
                            Description = e.[1].Title })
     |> Seq.toArray
 
-let s2 = new HtmlProvider<Season2>()
-let s2eps = 
-    s2.Tables.Episodes.Rows 
-    |> Seq.chunkBySize 2 
-    |> Seq.map (fun e -> { Title = e.[0].Title |> String.collect (fun c -> if c = '"' then "" else string c)
-                           Season = 2
-                           Number = e.[0].``No. in season`` |> int
-                           Description = e.[1].Title })
-    |> Seq.toArray
-
-let s3 = new HtmlProvider<Season3>()
-let s3eps = 
-    s3.Tables.Episodes.Rows 
-    |> Seq.chunkBySize 2 
-    |> Seq.map (fun e -> { Title = e.[0].Title |> String.collect (fun c -> if c = '"' then "" else string c)
-                           Season = 3
-                           Number = e.[0].``No. in season`` |> int
-                           Description = e.[1].Title })
-    |> Seq.toArray
-
-// Format has changed fro season 4 and 5-------------------------------------------
-
-//let s4 = new HtmlProvider<Season4>()
-//let s4eps = 
-//    s4.Tables.Episodes.Rows
-//    |> Seq.chunkBySize 2 
-//    |> Seq.map (fun e -> { Title = e.[0].Title |> String.collect (fun c -> if c = '"' then "" else string c)
-//                           Season = 4
-//                           Number = e.[0].``No. in season`` |> int
-//                           Description = e.[1].Title })
-//    |> Seq.toArray
-
-//let s5 = new HtmlProvider<Season5>()
-//let s5eps = 
-//    s5.Tables.Episodes.Rows 
-//    |> Seq.chunkBySize 2 
-//    |> Seq.map (fun e -> { Title = e.[0].Title |> String.collect (fun c -> if c = '"' then "" else string c)
-//                           Season = 5
-//                           Number = e.[0].``No. in season`` |> int
-//                           Description = e.[1].Title })
-//    |> Seq.toArray
-
 let printEpisodes (episodes : Episode array) =
     episodes
     |> Array.iter (fun e -> printfn "%A\r\n" e)
 
+let s1 = EpisodeInfo.Load(Season1) |> getEpisodes
+let s2 = EpisodeInfo.Load(Season2) |> getEpisodes
+let s3 = EpisodeInfo.Load(Season3) |> getEpisodes
+
 // Test
-[| s1eps; s2eps; s3eps |] |> Array.concat |> printEpisodes
+[| s1; s2; s3 |] |> Array.concat |> printEpisodes
 
