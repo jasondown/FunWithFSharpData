7d435dfa5a2143fb43830d6fae211750efa591a5
Jason Down <jdown@igloosoftware.com>
2018-01-10 02:15:00 -0500

    Have some better looking graphs using World Bank Provider (async).

--- FunWithFSharpData/WorldBankProvider.fsx
+++ FunWithFSharpData/WorldBankProvider.fsx
@@ -8,16 +8,36 @@ type WorldBank = WorldBankDataProvider<"World Development Indicators", Asynchron
 
 let data = WorldBank.GetDataContext()
 
-let countries =
-    [| data.Countries.Canada
-       data.Countries.``United States``
-       data.Countries.China
-       data.Countries.India |]
+let canPops = 
+    [| data.Countries.Canada.Indicators.``Population, total``
+       data.Countries.Canada.Indicators.``Population, female``
+       data.Countries.Canada.Indicators.``Population, male`` |]
 
-[| for c in countries -> 
-    c.Indicators.``Population, female``, c.Indicators.``Population, male`` |]
-|> Async.Parallel
-|> Async.RunSynchronously
-|> Array.map Chart.Line
-|> Chart.Combine
+let usPops =
+    [| data.Countries.``United States``.Indicators.``Population, total``
+       data.Countries.``United States``.Indicators.``Population, female``
+       data.Countries.``United States``.Indicators.``Population, male`` |]
+
+let ukPops =
+    [| data.Countries.``United Kingdom``.Indicators.``Population, total``
+       data.Countries.``United Kingdom``.Indicators.``Population, female``
+       data.Countries.``United Kingdom``.Indicators.``Population, male`` |]
+
+let getPopsChart (country : string) (populationInterval : float) (pops : Async<Runtime.WorldBank.Indicator> array) =
+    [| for p in pops -> p |]
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> Array.map (fun p -> Chart.Column(p, Name = sprintf "%s (%s)" p.Name country ))
+    |> Chart.Combine
+    |> Chart.WithXAxis (Title = "Year", TitleFontSize = 14.0, LabelStyle = ChartTypes.LabelStyle(Angle = -45, Interval = 5.0))
+    |> Chart.WithYAxis (Title = "Population", TitleFontSize = 14.0, LabelStyle = ChartTypes.LabelStyle(Interval = populationInterval))
+    |> Chart.With3D()
+    |> Chart.WithTitle (sprintf "Population per year - %s" country, InsideArea = false)
+    |> Chart.WithLegend(Title = "Legend", Docking = ChartTypes.Docking.Right, InsideArea = false)
+
+// Test it out
+[ (canPops  |> getPopsChart "CAN" 5_000_000.0)
+  (usPops   |> getPopsChart "US" 50_000_000.0)
+  (ukPops   |> getPopsChart "UK" 10_000_000.0) ] 
+|> Chart.Rows
 |> Chart.Show
