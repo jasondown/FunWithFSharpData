c558eb5ab1727aca97ded707f6458e898d32d94c
Jason Down <jdown@igloosoftware.com>
2018-01-09 23:58:49 -0500

    Added world bank type provider example.

---
+++ FunWithFSharpData/WorldBankProvider.fsx
@@ -0,0 +1,20 @@
+﻿#r "../packages/FSharp.Data.2.4.3/lib/net45/FSharp.Data.dll"
+#r "..//packages/FSharp.Charting.0.91.1/lib/net45/FSharp.Charting.dll"
+
+open FSharp.Data
+open FSharp.Charting
+
+let data = WorldBankData.GetDataContext()
+
+let countries =
+    [| data.Countries.Canada
+       data.Countries.``United States``
+       data.Countries.China
+       data.Countries.India |]
+
+[ for c in countries -> 
+    c.Indicators.``Population, female`` ]
+|> Async.Parallel
+|> Async.RunSynchronously
+|> Array.map Chart.Line
+|> Chart.Combine
