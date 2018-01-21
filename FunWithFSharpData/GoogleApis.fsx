06aa0087ac904e18c7b0b2d78134231270481125
Jason Down <jdown@igloosoftware.com>
2018-01-20 23:46:22 -0500

    Initial code for exploring Google APIs using F# Json Type Provider.

---
+++ FunWithFSharpData/GoogleApis.fsx
@@ -0,0 +1,17 @@
+﻿
+#r "../packages/FSharp.Data.2.4.3/lib/net45/FSharp.Data.dll"
+open FSharp.Data
+open System
+
+[<Literal>]
+let allApis = "https://www.googleapis.com/discovery/v1/apis"
+
+type GoogleApis = JsonProvider<allApis>
+
+let apis = GoogleApis.GetSample()
+
+let printApiDetails (apis : GoogleApis.Root) =
+    apis.Items
+    |> Array.iter (fun a -> printfn "%A" a)
+
+apis |> printApiDetails
