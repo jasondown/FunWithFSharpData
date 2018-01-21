242f8f312fa6541db90d1699f8b45796c45ef0bf
Jason Down <jdown@igloosoftware.com>
2018-01-21 00:08:50 -0500

    Nice summary representation of google apis now printed.

--- FunWithFSharpData/GoogleApis.fsx
+++ FunWithFSharpData/GoogleApis.fsx
@@ -1,5 +1,4 @@
-﻿
-#r "../packages/FSharp.Data.2.4.3/lib/net45/FSharp.Data.dll"
+﻿#r "../packages/FSharp.Data.2.4.3/lib/net45/FSharp.Data.dll"
 open FSharp.Data
 open System
 
@@ -7,11 +6,36 @@ open System
 let allApis = "https://www.googleapis.com/discovery/v1/apis"
 
 type GoogleApis = JsonProvider<allApis>
-
 let apis = GoogleApis.GetSample()
 
-let printApiDetails (apis : GoogleApis.Root) =
-    apis.Items
-    |> Array.iter (fun a -> printfn "%A" a)
+type ApiSummary = {
+    Title : string
+    Description : string
+    Documentation : string
+    }
 
-apis |> printApiDetails
+let getApiSummaries (apis : GoogleApis.Root) =
+    apis.Items
+    |> Array.map ( fun a ->
+        let desc = match a.Description with
+                   | Some d -> d
+                   | None -> ""
+        let doc = match a.DocumentationLink with
+                  | Some d -> d
+                  | None -> ""
+        { Title = a.Title
+          Description = desc
+          Documentation = doc
+        })
+
+let printApiSummaries (apis : ApiSummary array) =
+    apis
+    |> Array.iter (fun a ->
+        printfn "--------------------"
+        printfn "Title: %s" a.Title
+        printfn "Description: %s" a.Description 
+        printfn "Documentation: %s" a.Documentation
+        printfn "--------------------")
+
+// Print out the api summaries
+apis |> (getApiSummaries >> printApiSummaries)
